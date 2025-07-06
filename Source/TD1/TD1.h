82b2a562bede34aff0cefc23767443ef2c675043
SneakersSO <sr.oliveira86@gmail.com>
2025-07-06 15:06:31 -0400

    Added data-driven wave and enemy management

--- Source/TD1/TD1.h
+++ Source/TD1/TD1.h
@@ -3,4 +3,35 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "TD1.generated.h"
 
+UENUM(BlueprintType)
+enum class EEnemyType : uint8
+{
+	None,
+	Green,
+	Purple,
+	Red,
+	Yellow
+};
+
+USTRUCT(BlueprintType)
+struct FWaveData : public FTableRowBase
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float StartTime;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	EEnemyType EnemyType;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 SpawnCount;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float SpawnDelay;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float DelayRemaining;
+};
